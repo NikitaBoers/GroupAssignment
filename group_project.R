17fdc8864e48309c036b163d6a31dd5c4bc78741
Nikita Boers <boers.nikita@gmail.com>
2019-01-23 15:23:01 +0100

    working version

--- group_project.R
+++ group_project.R
@@ -6,43 +6,38 @@ library(plyr)
 canadians2 <- read.csv('new_canada_only2.csv')
 
 #Make sure birth dates are recognised as dates
-canadians_adapted <- canadians2 %>%
+canadians_adapted <- canadians2 %>% 
   mutate(
     dates = as.Date(birthDate),
     months = format(dates,'%m')
   )
+canadians_adapted <- canadians_adapted %>%
+  mutate( 
+    month_text = case_when( 
+      months == '01' ~ 'January', 
+      months == '02' ~ 'February', 
+      months == '03' ~ 'March', 
+      months == '04' ~ 'April', 
+      months == '05' ~ 'May', 
+      months == '06' ~ 'June', 
+      months == '07' ~ 'July', 
+      months == '08' ~ 'August', 
+      months == '09' ~ 'September', 
+      months == '10' ~ 'October', 
+      months == '11' ~ 'November', 
+      months == '12' ~ 'December' 
+    ) 
+  )
 
-canadians_adapted$months <- revalue(canadians_adapted$months, c("01"="1"))
-canadians_adapted$months <- revalue(canadians_adapted$months, c("02"="2"))
-canadians_adapted$months <- revalue(canadians_adapted$months, c("03"="3"))
-canadians_adapted$months <- revalue(canadians_adapted$months, c("04"="4"))
-canadians_adapted$months <- revalue(canadians_adapted$months, c("05"="5"))
-canadians_adapted$months <- revalue(canadians_adapted$months, c("06"="6"))
-canadians_adapted$months <- revalue(canadians_adapted$months, c("07"="7"))
-canadians_adapted$months <- revalue(canadians_adapted$months, c("08"="8"))
-canadians_adapted$months <- revalue(canadians_adapted$months, c("09"="9"))
+canadians_adapted$month_text <- factor(canadians_adapted$month_text, levels = c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
 
-#Compute distribution of astrological signs within Canadian hockey players & plot
-ggplot(data = dataframe) +
-  geom_bar(stat = 'count', mapping = aes(x=astrologicalsign, fill = ?))
+ggplot() +
+  geom_bar(data=canadians_adapted, mapping=aes(x=month_text, fill=month_text), colour='black') +
+  scale_fill_brewer(palette="Set3") +
+  scale_fill_discrete(name="Months") +
+  labs(title = "Number of Canadian ice hockey players \nbased on their month of birth, born in 1870-1999", x = "Months", y = "Number of players")
 
-canadians_adapted2$abbmonths <- factor(canadians_adapted2$month_text, levels = c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
 
-#plots
-#bargraph
-ggplot(data = canada) + 
-  geom_bar(mapping = aes(x = month))
-
-#map
-library('maps')
-library('ggthemes')
-#create data to use
-citydata<- dataframe%>%group_by(city)%>%summarise(sum(count=count,na.rm=TRUE))
-
-canadamap= Germanymap<-map_data('world')%>% filter(region=="Canada")
-ggplot(data = dataframe)+ 
-  geom_polygon(data = canadamap, mapping = aes(x=long, y=lat, group=group))+
-  geom_point(data= citydata, mapping = aes (x=long, y=lat, size=count )
 
 
              #Same thing for subgroups based on birthyear
