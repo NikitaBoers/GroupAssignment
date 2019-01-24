b74aebfa3e73fdd85bafd5ef3fc70059ea11fa4b
ReinAvalerion <46708506+ReinAvalerion@users.noreply.github.com>
2019-01-24 11:05:34 +0100

    Update salaries.R

--- salaries.R
+++ salaries.R
@@ -7,41 +7,6 @@ salary_finland <- read.csv('salary_finland.csv')
 salary_sweden <- read.csv('salary_finland.csv')
 salary_usa <- read.csv('salary_usa.csv')
 
-#copy birth months plot Canada
-canadians2 <- read.csv('new_canada_only2.csv')
-
-canadians_adapted <- canadians2 %>% 
-  mutate(
-    dates = as.Date(birthDate),
-    months = format(dates,'%m')
-  )
-
-canadians_adapted <- canadians_adapted %>%
-  mutate( 
-    month_text = case_when( 
-      months == '01' ~ 'January', 
-      months == '02' ~ 'February', 
-      months == '03' ~ 'March', 
-      months == '04' ~ 'April', 
-      months == '05' ~ 'May', 
-      months == '06' ~ 'June', 
-      months == '07' ~ 'July', 
-      months == '08' ~ 'August', 
-      months == '09' ~ 'September', 
-      months == '10' ~ 'October', 
-      months == '11' ~ 'November', 
-      months == '12' ~ 'December' 
-    ) 
-  )
-
-canadians_adapted$month_text <- factor(canadians_adapted$month_text, levels = c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
-
-ggplot() +
-  geom_bar(data=canadians_adapted, mapping=aes(x=month_text, fill=month_text), colour='black') +
-  scale_fill_brewer(palette="Set3") +
-  scale_fill_discrete(name="Months") +
-  labs(title = "Number of Canadian ice hockey players \nbased on their month of birth, born in 1870-1999", x = "Months", y = "Number of players")
-
 #compute mean salaries per month
 can_salary_adapted <- salary_canada %>% 
   mutate(
